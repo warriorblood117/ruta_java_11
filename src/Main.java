42ebef118a51482c4f18d5f028dca24366a0261b
jhfranco2 <jhoanmateofranco@gmail.com>
2023-10-14 13:06:38 -0500

    modified: se corrije el editar y se añade la verificación para que la persona tampoco este duplicada

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         String document, name, lastname, phoneNumber, mail;
@@ -22,6 +24,7 @@ public class Main {
             System.out.println(
                     "Seleccione una acción:\n1: Agregar persona\n2: Editar persona\n3: Mostrar personas\n4: eliminar Persona\n5:salir");
             int opcion = entrada.nextInt();
+            entrada.nextLine();
             switch (opcion) {
                 case 1:
                     name = Utilities.capturarString("Ingrese por favor el nombre");
@@ -64,13 +67,9 @@ public class Main {
                 case 2:
                     System.out.println("Ingrese el índice de la persona que desea editar: ");
                     int indice = entrada.nextInt();
-                    entrada.nextLine(); // Limpia el salto de línea
+                    entrada.nextLine(); // Consumir el salto de línea
+                    listaPersonas = editarPersona(listaPersonas, indice);
 
-                    if (indice >= 0 && indice < listaPersonas.size()) {
-                        listaPersonas = editarPersona(listaPersonas, indice);
-                    } else {
-                        System.out.println("Índice no válido.");
-                    }
                     break;
                 case 3:
                     imprimirContactos(listaPersonas);
@@ -84,6 +83,7 @@ public class Main {
                     break;
                 case 5:
                     System.out.println("Saliendo del programa");
+                    entrada.close(); // Cerrar el Scanner al salir del programa
                     System.exit(0);
                 default:
                     System.out.println("No es una opción valida");
@@ -97,36 +97,66 @@ public class Main {
         System.out.println("Personas Ingresadas:");
         for (int i = 0; i < listaPersonas.size(); i++) {
             Persona persona = listaPersonas.get(i);
-            System.out.println(persona);
+            System.out.println("Índice: " + i + ", Nombre: " + persona.getName() + ", Apellido: " + persona.getLastname() + ", documento: " + persona.getDocument());
         }
     }
 
     public static List<Persona> editarPersona(List<Persona> listaPersonas, int indice) {
         String document, name, lastname, phoneNumber, mail;
-
         int age;
 
-        try (Scanner entrada = new Scanner(System.in)) {
-            name = Utilities.capturarString("Ingrese por favor el nombre");
-            document = Utilities.capturarString("Ingrese por favor el documento");
-            lastname = Utilities.capturarString("Ingrese por favor el apellido");
-            phoneNumber = Utilities.capturarString("Ingrese por favor el telefono");
-            mail = Utilities.capturarString("Ingrese por favor el mail");
-            age = Utilities.capturarInt("Ingrese por favor la edad");
-            Persona personaNueva = new Persona(document, name, lastname, phoneNumber, age, mail);
-            personaNueva.setAge(age);
-            personaNueva.setDocument(document);
-            personaNueva.setName(name);
-            personaNueva.setLastname(lastname);
-            personaNueva.setPhoneNumber(phoneNumber);
-            personaNueva.setMail(mail);
+        Scanner entrada = new Scanner(System.in); // Eliminar el try-with-resources
 
-            Persona personaEditada = new Persona(document, name, lastname, phoneNumber, age, mail);
+        name = Utilities.capturarString("Ingrese por favor el nombre");
+        document = Utilities.capturarString("Ingrese por favor el documento");
+        lastname = Utilities.capturarString("Ingrese por favor el apellido");
+        phoneNumber = Utilities.capturarString("Ingrese por favor el telefono");
+        mail = Utilities.capturarString("Ingrese por favor el mail");
+        age = Utilities.capturarInt("Ingrese por favor la edad");
+        Persona personaEditada = new Persona(document, name, lastname, phoneNumber, age, mail);
+        personaEditada.setAge(age);
+        personaEditada.setDocument(document);
+        personaEditada.setName(name);
+        personaEditada.setLastname(lastname);
+        personaEditada.setPhoneNumber(phoneNumber);
+        personaEditada.setMail(mail);
+        listaPersonas.set(indice, personaEditada);
+        personaEditada.setAge(age);
+        personaEditada.setDocument(document);
+        personaEditada.setName(name);
+        personaEditada.setLastname(lastname);
+        personaEditada.setPhoneNumber(phoneNumber);
+        personaEditada.setMail(mail);
+        String validarEmail = Utilities.isValidEmail(personaEditada.getMail());
+        System.out.println(validarEmail);
+        String validatePhoneNumber = Utilities.isValidPhoneNumber(personaEditada.getPhoneNumber());
+        System.out.println(validatePhoneNumber);
+        boolean personaDuplicada = true;
+        while ( !personaDuplicada){
+        for (Persona p : listaPersonas) {
+            if (p.getDocument().equals(document) &&
+                    p.getName().equals(name) &&
+                    p.getLastname().equals(lastname) &&
+                    p.getPhoneNumber().equals(phoneNumber) &&
+                    p.getMail().equals(mail)) {
+                System.out.println(" la persona esta duplicada");
 
-            listaPersonas.set(indice, personaEditada);
+            }
+            personaDuplicada = false;
         }
+        }
+
+        if (personaDuplicada) {
+            System.out.println("Persona duplicada. No se ha agregado a la lista.");
+        } else {
+            listaPersonas.add(personaEditada);
+            System.out.println("Persona agregada con éxito.");
+        }
+
+
         return listaPersonas;
     }
+
 
     public static void eliminarPersona(List<Persona> listaPersonas, int indice) {
         if (indice >= 0 && indice < listaPersonas.size()) {
